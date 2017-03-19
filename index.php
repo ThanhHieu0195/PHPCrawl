71426912d57edeaf84f4c2fd9b5b8bbb76ba9ea0
Tran Cao Thanh hieu <hieutct@solazu.com>
2017-03-19 22:45:23 +0700

    v4

--- index.php
+++ index.php
@@ -6,41 +6,46 @@ $MAX_URL = 5000;
 $LINK_URL_HTML = '';
 include("libs/PHPCrawler.class.php");
 include_once 'simple_html_dom.php';
+$myfile = fopen("html/downloadList.txt", "w") or die("Unable to open file!");
 
 class MyCrawler extends PHPCrawler
 {
-  function handleDocumentInfo($DocInfo) 
-  {
-    global $INDEX, $LINK_URL_HTML;
-    if (PHP_SAPI == "cli") $lb = "\n";
-    else $lb = "<br />";
-    echo "Page requested: ".$DocInfo->url." (".$DocInfo->http_status_code.")".$lb;
-    
-    echo "Referer-page: ".$DocInfo->referer_url.$lb;
+    function handleDocumentInfo($DocInfo)
+    {
+        global $INDEX, $LINK_URL_HTML, $myfile;
+        if (PHP_SAPI == "cli") $lb = "\n";
+        else $lb = "<br />";
+        echo "Page requested: ".$DocInfo->url." (".$DocInfo->http_status_code.")".$lb;
 
-    if ($DocInfo->received == true){
-        echo "Content received: ".$DocInfo->bytes_received." bytes".$lb;
+        echo "Referer-page: ".$DocInfo->referer_url.$lb;
 
-        $html = file_get_html($DocInfo->url);
-        $name_file_html = 'html/'.$INDEX.'.html';
-        $html->save($name_file_html);
-        $row_html = $INDEX.'. '.$DocInfo->url."\n";
-        $INDEX++;
-        $LINK_URL_HTML .= $row_html;
+        if ($DocInfo->received == true){
+            echo "Content received: ".$DocInfo->bytes_received." bytes".$lb;
+            try {
+                $html = file_get_html($DocInfo->url);
+                $name_file_html = 'html/'.$INDEX.'.html';
+                $html->save($name_file_html);
+                $row_html = $INDEX.'. '.$DocInfo->url."\n";
+                $INDEX++;
+                $txt = $row_html;
+                fwrite($myfile, $txt);
+            } catch (Exception $e) {
+                echo 'error';
+            }
+        }
+        else
+            echo "Content not received".$lb;
+
+
+        echo $lb;
+
+        flush();
     }
-    else
-      echo "Content not received".$lb; 
-    
-
-    echo $lb;
-    
-    flush();
-  } 
 }
 
 $crawler = new MyCrawler();
 
-$crawler->setURL("http://vnexpress.net/");
+$crawler->setURL("http://dantri.com.vn/");
 
 $crawler->addContentTypeReceiveRule("#text/html#");
 
@@ -56,15 +61,12 @@ $report = $crawler->getProcessReport();
 
 if (PHP_SAPI == "cli") $lb = "\n";
 else $lb = "<br />";
-    
+
 echo "Summary:".$lb;
 echo "Links followed: ".$report->links_followed.$lb;
 echo "Documents received: ".$report->files_received.$lb;
 echo "Bytes received: ".$report->bytes_received." bytes".$lb;
 echo "Process runtime: ".$report->process_runtime." sec".$lb;
 
-$myfile = fopen("html/downloadList.txt", "w") or die("Unable to open file!");
-$txt = $LINK_URL_HTML;
-fwrite($myfile, $txt);
 fclose($myfile);
 ?>
